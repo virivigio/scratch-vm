abe993fa61bd347e6bfff78a7b1df22bf431a5ab
Virgilio Pierini <virgilio.pierini@clickode.it>
2020-03-22 21:47:22 +0100

    Adds microbit more

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,8 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    mentor: () => require('../extensions/scratch3_mentor')
+    mentor: () => require('../extensions/scratch3_mentor'),
+    microbitMore: () => require('../extensions/scratch3_microbit_more')
 };
 
 /**
